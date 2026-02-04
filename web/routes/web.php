09a0543cfcc060b8473b6d6f95f04e3ab4028b5a
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-04 09:38:24 +0100

    feat: add restaurateur routes

--- web/routes/web.php
+++ web/routes/web.php
@@ -15,6 +15,16 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/restaurant/profile', function(){
+        return view('restaurateur.profile.index');
+    })->name('restaurant.profile');
 });
+
+    Route::get('/restaurant', function(){
+        return view('restaurateur.dashboard');
+    })->name('restaurateur.dashboard');
+
+    Route::resource('restaurants', App\Http\Controllers\RestaurantController::class);
+    Route::get('/reservations', [App\Http\Controllers\ReservationController::class, 'index'])->name('reservations.index');
 
 require __DIR__.'/auth.php';
